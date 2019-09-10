cb3a3be64fe0900a5fe6f777851f00e9a28415cc
ericagleaton <EricaGleaton@comcast.net>
2019-09-10 10:56:12 -0400

    EJG 09/10/2019 Add random Number for the secret number

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,13 +2,15 @@ package com.company;
 //Repeating yourself with a while loop
 
 import java.util.Scanner;
+import java.util.Random;
+
 
 public class Main {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         int secretNumber, guess = 0, counter=0;//guess is initialized to 0
-
-        secretNumber = 6;
+        Random random = new Random();
+        secretNumber = random.nextInt(10);
 
         System.out.println("I'm thinking of a number between 1 and 10");
         System.out.print("Enter the number:");
@@ -29,7 +31,7 @@ public class Main {
         }
 
         System.out.println("Your guess " + guess + " is correct. You win a prize!");
-        System.out.println("It took " + (counter+1) + " to guess the correct number.");
+        System.out.println("It took " + (counter+1) + " guesses to win.");
         keyboard.close();
     }
 }
